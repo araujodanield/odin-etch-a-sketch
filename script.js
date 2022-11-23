e4d58fcb7ac635e15d2961b3c04a6998f607246f
araujodanield <daniel-araujo2021@outlook.com>
2022-11-23 12:23:18 -0300

    Add the 'createPixels' function which creates a quantity of  divs in the canvas based on the user input

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log ('test')
+
+function createPixels(size) {
+    const canvas = document.getElementById("canvas");
+    canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    
+    for (let i = 0; i < size * size; i++) {
+        let pixel = document.createElement("div");
+        canvas.appendChild(pixel).classList.add("pixel");
+    }
+}
+
+createPixels(25);
