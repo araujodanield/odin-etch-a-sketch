c4d0f06cb96c245d08d78e2d02110cb58112d08f
araujodanield <daniel-araujo2021@outlook.com>
2022-11-23 22:31:01 -0300

    Add a initial logic  to the drawing mechanic

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+const canvas = document.getElementById("canvas");
 
 function createPixels(size) {
-    const canvas = document.getElementById("canvas");
     canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${size} , 1fr)`;
     
     for (let i = 0; i < size * size; i++) {
         let pixel = document.createElement("div");
         canvas.appendChild(pixel).classList.add("pixel");
+        pixel.addEventListener("dragover", () => {
+            pixel.style.backgroundColor = "black";
+        })
     }
 }
 
-createPixels(25);
+createPixels(65);
