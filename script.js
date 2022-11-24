7ca465371c9dd068fb90c41e6da5026df17fcee6
araujodanield <daniel-araujo2021@outlook.com>
2022-11-24 00:23:51 -0300

    Fix the drawing mechanic to work only when mouse is pressed

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const body = document.querySelector("body");
 const canvas = document.getElementById("canvas");
+let mouseIsClicked = false;
+
+
+body.addEventListener("mousedown", () => {
+    mouseIsClicked = !mouseIsClicked;
+});
+body.addEventListener("mouseup", () => {
+    mouseIsClicked = false;
+});
 
 function createPixels(size) {
     canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
@@ -6,9 +16,17 @@ function createPixels(size) {
     
     for (let i = 0; i < size * size; i++) {
         let pixel = document.createElement("div");
+        pixel.style.border = "1px solid rgba(0, 0, 0, 0.1)";
+        
         canvas.appendChild(pixel).classList.add("pixel");
-        pixel.addEventListener("dragover", () => {
+
+        pixel.addEventListener("click", () => {
             pixel.style.backgroundColor = "black";
+        })
+        pixel.addEventListener("mouseover", () => {
+            if (mouseIsClicked) {
+                pixel.style.backgroundColor = "black";
+            }
         })
     }
 }
