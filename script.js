e9ab1515e0b47f84561153a6494da4f46c192a66
araujodanield <daniel-araujo2021@outlook.com>
2022-12-01 19:05:49 -0300

    Add the logic to change canvas pixel amount, but with a bug that break the drawing functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body");
 const canvas = document.getElementById("canvas");
 
-const clearBtn = document.createElement("button");
-clearBtn.textContent = "Clear";
+const colorBtn = document.createElement("input");
+colorBtn.type = "color";
 const blackBtn = document.createElement("button");
 blackBtn.textContent = "Black";
 const randomBtn = document.createElement("button");
@@ -11,15 +11,26 @@ const shadowBtn = document.createElement("button");
 shadowBtn.textContent = "Shadow Effect";
 const eraserBtn = document.createElement("button");
 eraserBtn.textContent = "Eraser";
+const clearBtn = document.createElement("button");
+clearBtn.textContent = "Clear";
+const sizeSelector = document.createElement("input");
+sizeSelector.type = "range";
+sizeSelector.min = 2;
+let number = document.createElement("p")
+number.textContent = "Select a size"
+number.style.color = "white"
 
-body.appendChild(clearBtn).classList.add("btn");
+body.appendChild(colorBtn).classList.add("btn");
 body.appendChild(blackBtn).classList.add("btn");
 body.appendChild(randomBtn).classList.add("btn");
 body.appendChild(shadowBtn).classList.add("btn");
 body.appendChild(eraserBtn).classList.add("btn");
+body.appendChild(clearBtn).classList.add("btn");
+body.appendChild(sizeSelector).classList.add("btn");
+body.appendChild(number)
 
 
-// Logic to apply on the canvas. By setting this, the user can only draw when he's holding the left mouse button.
+// Logic to apply on the canvas. By setting this, the user can only draw when the left mouse button is pressed.
 let mouseIsClicked;
 body.addEventListener("mousedown", () => {
     mouseIsClicked = true;
@@ -40,10 +51,26 @@ function createPixels(size) {
         canvas.appendChild(px).classList.add("pixel");
     }
 };
-createPixels(35);
+createPixels(16);
+
+function sizeSelect() {
+    sizeSelector.addEventListener("mousemove", () => {
+        number.textContent = sizeSelector.value + " X " + sizeSelector.value;
+    })
+    sizeSelector.addEventListener("change", () => {
+        const pixelAmount = body.querySelectorAll(".pixel")
+        pixelAmount.forEach (currentPx => {
+            currentPx.remove();
+        });
+        createPixels(sizeSelector.value)
+    })
+};
+sizeSelect();
+
+
+const pixels = document.querySelectorAll(".pixel");
 
 // The default pen when the page is loaded.
-const pixels = document.querySelectorAll(".pixel");
 pixels.forEach (pixel => {
     pixel.addEventListener("mousedown", () => {
         pixel.style.backgroundColor = "black";
@@ -55,16 +82,23 @@ pixels.forEach (pixel => {
     })
 });
 
-// Clear the canvas by removing the color of all pixels
-function clearCanvas() {
-    clearBtn.addEventListener("click", () => {
+// Allow the user to select a custom color for the pen and copy colors on the page.
+function colorPicker() {
+    colorBtn.addEventListener("click", () => {
         pixels.forEach (pixel => {
-            pixel.style.backgroundColor = "";
+            pixel.addEventListener("mousedown", () => {
+                pixel.style.backgroundColor = colorBtn.value;
+            })
+            pixel.addEventListener("mouseover", () => {
+                if (mouseIsClicked) {
+                    pixel.style.backgroundColor = colorBtn.value;
+                }
             })
         })
+    })
 };
 
-// A simple black pen
+// A simple black pen.
 function blackPen() {
     blackBtn.addEventListener("click", () => {
         pixels.forEach (pixel => {
@@ -80,7 +114,8 @@ function blackPen() {
     })
 };
 
-// Add a random color for each pixel on the interaction. To add a new color to a pixel that was already painted you need to click the button again.
+
+// Add a random color for each pixel on the interaction. To add a new color to a pixel that was already painted the user need to click the button again.
 function randomPen() {
     randomBtn.addEventListener("click", () => {
         pixels.forEach (pixel => {
@@ -100,7 +135,7 @@ function randomPen() {
 };
 
 // A pen that adds 0.1 opacity to the pixel and increases this value in each interaction.
-// OBS: There are a lot of ways to make this logic with fewer lines, but due I'm trying to make this the most "beginner friendly" and visual possible, I opted for an if else statement.
+// OBS: There are lot of ways to make this logic with fewer lines, but due I'm trying to make this the most "beginner friendly" and visual possible, I opted for an if else statement.
 // It's possible to make this same "visual" logic using the switch statement, but it will be a little longer than the if else.
 function shadowPen() { 
     shadowBtn.addEventListener("click", () => {
@@ -156,7 +191,7 @@ function shadowPen() {
     })
 };
 
-// Eraser
+// Allow the user to delete pixels color individually.
 function pixelEraser() {
     eraserBtn.addEventListener("click", () => {
         pixels.forEach (pixel => {
@@ -172,9 +207,18 @@ function pixelEraser() {
     })
 };
 
+// Clear the canvas by removing the color of all pixels.
+function clearCanvas() {
+    clearBtn.addEventListener("click", () => {
+        pixels.forEach (pixel => {
+            pixel.style.backgroundColor = "";
+        })
+    })
+};
 
-clearCanvas();
+colorPicker();
 blackPen();
 randomPen();
 shadowPen();
 pixelEraser();
+clearCanvas();
